dff09c88a7ce1477634388b9d0f2c993474c868d
SamuelFerreira1192 <65785806+SamuelFerreira1192@users.noreply.github.com>
2024-01-21 19:09:41 -0300

    Add files via upload

---
+++ SamuelFerreira1192/jellyfin-plugin-m3ueditor-master/Configuration/PluginConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MediaBrowser.Model.Plugins;
+using System;
+
+namespace Jellyfin.Plugin.M3UEditor.Configuration
+{
+    public enum SomeOptions
+    {
+        OneOption,
+        AnotherOption
+    }
+
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public string API_KEY { get; set; }
+
+        public PluginConfiguration()
+        {
+            API_KEY = Guid.NewGuid().ToString();
+        }
+    }
+}
