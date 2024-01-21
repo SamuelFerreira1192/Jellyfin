9c696a77e190f115081258e3cfdbb33627fcf0a3
SamuelFerreira1192 <65785806+SamuelFerreira1192@users.noreply.github.com>
2024-01-21 19:11:02 -0300

    Add files via upload

---
+++ SamuelFerreira1192/jellyfin-plugin-m3ueditor-master/Entities/M3UItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jellyfin.Plugin.M3UEditor.Entities
+{
+    public class M3UItem
+    {
+        public string b64 { get; set; }
+        public string Url { get; set; }
+        public string ExtInf { get; set; }
+        public string Name { get; set; }
+        public bool hidden { get; set; }
+        public Dictionary<string, string> Attributes { get; set; }
+        
+    }
+}
---
+++ SamuelFerreira1192/jellyfin-plugin-m3ueditor-master/Entities/M3UPlaylist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jellyfin.Plugin.M3UEditor.Entities
+{
+    public class M3UPlaylist
+    {
+        public string PlaylistName { get; set; }
+        public string PlaylistUrl { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
